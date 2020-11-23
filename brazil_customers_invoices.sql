9e25f904fe7a28774f09bad31e7c225152397059
LukeZvada <zvadamusic@gmail.com>
2020-11-23 11:23:57 -0500

    brazilCustomerInvoices done..

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,4 @@
+SELECT FirstName || '' || Lastname as FullName, InvoiceDate, BillingCountry, InvoiceId
+FROM Invoice as i
+JOIN Customer as c ON c.CustomerId = i.CustomerId
+AND c.Country = "Brazil"
