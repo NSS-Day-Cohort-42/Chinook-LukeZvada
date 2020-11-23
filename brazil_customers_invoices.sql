7e9c2fa92a77bea35555718a6e2e15809ceec239
LukeZvada <zvadamusic@gmail.com>
2020-11-23 12:35:16 -0500

    added space between ' '

--- brazil_customers_invoices.sql
+++ brazil_customers_invoices.sql
@@ -1,4 +1,4 @@
-SELECT FirstName || '' || Lastname as FullName, InvoiceDate, BillingCountry, InvoiceId
+SELECT FirstName || ' ' || Lastname as FullName, InvoiceDate, BillingCountry, InvoiceId
 FROM Invoice as i
 JOIN Customer as c ON c.CustomerId = i.CustomerId
 AND c.Country = "Brazil"
--- sales_agent_invoices.sql
+++ sales_agent_invoices.sql
@@ -1,7 +1,8 @@
 SELECT 
-e.FirstName || '' || e.LastName as EmployeeFullName,
+e.FirstName || ' ' || e.LastName as EmployeeFullName,
 i.InvoiceId, i.InvoiceDate, i.BillingAddress, i.BillingCity,
 i.BillingState, i.BillingCountry, i.BillingPostalCode, i.Total
 FROM Employee as e
 JOIN Customer as c ON c.SupportRepId = e.EmployeeId
 JOIN Invoice as i ON i.CustomerId = c.CustomerId
+ORDER BY EmployeeFullName DESC
